962a95bfeef0e7dcb2d108f3a0097be4e4e06bb5
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-10-25 22:41:24 -0300

    Estruturando home e adicionando titulo

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,32 @@
+import 'package:flutter/material.dart';
 
+main() {
+  return runApp(const ExpensesApp());
+}
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
+      ),
+      body: const Center(
+        child: Text('Versão inicial'),
+      ),
+    );
+  }
+}
