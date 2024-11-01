739b293d1f2dcdcbf1009f0a1486264a9c9405ca
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-11-01 20:18:11 -0300

    formatando dada

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:despesaspessoais/models/transaction.dart';
+import 'package:intl/intl.dart';
+
 import 'package:flutter/material.dart';
 
 main() {
@@ -57,8 +59,43 @@ class MyHomePage extends StatelessWidget {
           Column(
               children: _transactions
                   .map((tr) => Card(
-                        child: Text(tr.title),
-                      ))
+                          child: Row(
+                        children: <Widget>[
+                          Container(
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 15),
+                            padding: const EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Colors.purple)),
+                            child: Text(
+                              'R\$ ${tr.value.toStringAsFixed(2)}',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.purple,
+                              ),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                tr.title,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                ),
+                              ),
+                              Text(
+                                DateFormat('dd MMM y').format(tr.date),
+                                style: const TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      )))
                   .toList())
         ],
       ),
