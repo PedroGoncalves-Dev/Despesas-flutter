8d3048276ca14220b88690b2de114c57eace42d2
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-11-06 23:27:07 -0300

    transformou em um stateFul pra que possa conseguir abrir o modal

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
-import 'package:despesaspessoais/components/transaction_user.dart';
+import 'package:despesaspessoais/components/transaction_form.dart';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
+
+import 'components/transaction_list.dart';
+import 'models/transaction.dart';
 
 main() {
   return runApp(const ExpensesApp());
@@ -20,14 +24,59 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tenis de corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz ',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextInt(100).toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
+  _openTransactionFormMofal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) => TransactionForm(onSubmit: _addTransaction));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Despesas Pessoais'),
+        actions: [
+          IconButton(
+            onPressed: () => _openTransactionFormMofal(context),
+            icon: const Icon(Icons.add),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         //faz com que possa rolar a tela
@@ -41,9 +90,14 @@ class MyHomePage extends StatelessWidget {
                 child: Text('Gráfico'),
               ),
             ),
-            TransactionUser(),
+            TransactionList(_transactions),
           ],
         ),
+      ),
+      //é usado pra deixar um botao de icone em baixo
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _openTransactionFormMofal(context),
+        child: const Icon(Icons.add),
       ),
     );
   }
