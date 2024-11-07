04edf196edb42868cabd07f32d0aaceef1fd58d0
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-11-07 14:35:21 -0300

    condição adicionada pra quando não houver nenhuma conta cadastrada

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -11,47 +11,63 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView(
-          children: transactions
-              .map((tr) => Card(
-                      child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 15),
-                        padding: const EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.purple)),
-                        child: Text(
-                          'R\$ ${tr.value.toStringAsFixed(2)}',
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                            color: Colors.purple,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                Text(
+                  'Nenhuma conta cadastrada',
+                  style: TextStyle(color: Theme.of(context).primaryColor),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                    height: 200,
+                    child: Image.asset('assets/images/waiting.png'))
+              ],
+            )
+          : ListView(
+              children: transactions
+                  .map((tr) => Card(
+                          child: Row(
                         children: <Widget>[
-                          Text(
-                            tr.title,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
+                          Container(
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 15),
+                            padding: const EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    color: Theme.of(context).primaryColor)),
+                            child: Text(
+                              'R\$ ${tr.value.toStringAsFixed(2)}',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Theme.of(context).primaryColor,
+                              ),
                             ),
                           ),
-                          Text(
-                            DateFormat('dd MMM y').format(tr.date),
-                            style: const TextStyle(
-                              color: Colors.grey,
-                            ),
-                          )
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                tr.title,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                ),
+                              ),
+                              Text(
+                                DateFormat('dd MMM y').format(tr.date),
+                                style: const TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              )
+                            ],
+                          ),
                         ],
-                      ),
-                    ],
-                  )))
-              .toList()),
+                      )))
+                  .toList()),
     );
   }
 }
