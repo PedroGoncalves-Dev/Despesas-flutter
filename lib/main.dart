3085d599f32f8ee4ea8635e0ce66975151b6712b
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-11-28 08:23:39 -0300

    alterando o botom de mostrar chart para appBar

--- lib/main.dart
+++ lib/main.dart
@@ -81,10 +81,19 @@ class _MyHomePageState extends State<MyHomePage> {
     final appBar = AppBar(
       title: const Text('Despesas Pessoais'),
       actions: [
+        if (isLandscape)
+          IconButton(
+            onPressed: () {
+              setState(() {
+                _showChart = !_showChart;
+              });
+            },
+            icon: Icon(_showChart ? Icons.list : Icons.pie_chart),
+          ),
         IconButton(
           onPressed: () => _openTransactionFormMofal(context),
           icon: const Icon(Icons.add),
-        )
+        ),
       ],
     );
     final availableHeight = MediaQuery.of(context).size.height -
@@ -98,21 +107,6 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            if (isLandscape)
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('Alterar'),
-                  Switch(
-                    value: _showChart,
-                    onChanged: (value) {
-                      setState(() {
-                        _showChart = value;
-                      });
-                    },
-                  ),
-                ],
-              ),
             if (_showChart || !isLandscape)
               SizedBox(
                 height:
